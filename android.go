bbf2bf5ad82f5a7d0bb56b46308aa8aa9eaf5526
luojie <xiaoyaojie710@163.com>
2020-03-19 16:40:07 +0800

    fix the nil pointer bug

--- android.go
+++ android.go
@@ -2,24 +2,25 @@ package unifiedrecharge
 
 import (
 	"context"
-	"fmt"
 	ap "google.golang.org/api/androidpublisher/v2"
 	"google.golang.org/api/option"
 	"sync"
 )
 
-//直接调用androidpushliser v2
+//call androidpushliser v2
 var (
-	once sync.Once
+	once            sync.Once
 	purchaseService *ap.PurchasesProductsService
+	lastError       error
 )
 
-func getService(configFile string) *ap.PurchasesProductsService{
+func getService(configFile string) *ap.PurchasesProductsService {
 	once.Do(func() {
 		ctx := context.Background()
 		service, err := ap.NewService(ctx, option.WithCredentialsFile(configFile))
 		if err != nil {
-			fmt.Println("new service err: ", err)
+			//fmt.Println("new service err: ", err)
+			lastError = err
 		}
 
 		purchaseService = ap.NewPurchasesProductsService(service)
@@ -29,5 +30,10 @@ func getService(configFile string) *ap.PurchasesProductsService{
 }
 
 func CheckGoogleAndroidPurchase(configFile, packageName, productId, token string) (productPurchase *ap.ProductPurchase, err error) {
-	return getService(configFile).Get(packageName, productId, token).Do()
+	service := getService(configFile)
+	if service == nil {
+		return nil, lastError
+	}
+
+	return service.Get(packageName, productId, token).Do()
 }
